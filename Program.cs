38c13f2af51586ecce3d7fa9ff46d5540d1cecb2
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-07 20:21:43 -0300

    [M1S01] Ex 3 - Boletim
    [Estrutura de Decisão]
    Crie um programa para calcular a média dos alunos e informar se o aluno foi aprovado, ficou de recuperação ou foi reprovado.
    
    O programa deve receber 3 notas e calcular a média entre elas utilize a seguinte fórmula para calcular a média: media = (nota1 + nota2 + nota3 )÷3.  após calcular a média, verifique se o aluno foi aprovado seguindo os critérios: 
    	Se nota maior ou igual 6 : Saída-> Aluno Aprovado com média {Valor da Média} 
    	Se nota maior ou igual 5, e menor que 6 : Saída-> Aluno em Recuperação com média {Valor da Média} 
    	Se nota menor que 5 : Saída-> Aluno Reprovado com média {Valor da Média}

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Calcular a média dos alunos e informar se foi aprovado, ficou em recuperação ou reprovado.
+float nota1;
+float nota2;
+float nota3;
+float media;
+
+Console.WriteLine("Olá! Vamos descobrir se você foi aprovado, reprovado ou ficou de recuperação na nossa disciplina!");
+Console.WriteLine("Digite a sua nota da primeira avaliação:");
+nota1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite a sua nota da segunda avaliação:");
+nota2 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite a sua nota da terceira avaliação:");
+nota3 = float.Parse(Console.ReadLine());
+media = (nota1 + nota2 + nota3) / 3;
+if (media >= 6){
+    Console.WriteLine($"Parabéns! Você foi aprovado com média {media}!!");
+}
+else if(media < 6 && media >= 5){
+    Console.WriteLine($"Poxa! Vi que você ficou em recuperação com média {media}!");
+}
+else {
+    Console.WriteLine($"Infelizmente, você foi reprovado com média {media}, mas não desista! Estude!");
+}
